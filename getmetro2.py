c960a8608c58aac716a2911e2ea2288606012e04
chemicode <brian@brian-thomson.com>
2015-04-12 19:10:27 -0400

    Handled exception for non-int board times.  Minor syntax changes.

--- getmetro2.py
+++ getmetro2.py
@@ -8,31 +8,35 @@
 #3-1-15 Added handling exceptions to deal with internet outages
 #November 2014 Created
 #Author: Brian Thomson
-import urllib
-#import urllib.request PYTHON3
+import urllib #PYTHON2
+#import urllib.request #PYTHON3
 import json
 import subprocess
 #public API key, please get your own for continuously repeated calls
 url="http://api.wmata.com/StationPrediction.svc/json/GetPrediction/B08?api_key=kfgpmgvfgacx98de9q3xazww"
 proc=subprocess.Popen(['perl','signmaster.pl'],stdin=subprocess.PIPE) #Uncomment before delpoying on PI
 s=""
+y=0
 try:
-        #Python2 next 2 lines - differences in URLLLIB
+        #Python2 next 2 lines
         response=urllib.urlopen(url)
         data=json.loads(response.read())['Trains']
         #Python3 next 2 lines - note that data request is now a bytestream
         #response=urllib.request.urlopen(url)
         #data=json.loads(response.read().decode('utf-8'))['Trains']
-        #print "Car".rjust(3),"Destination".rjust(10),"Min".rjust(3)
-        for x in range(0, len(data)):
-                #I can't make it to the station in < 6 minutes!
-                if int(data[x]['Min'])>=6:
-                        s+=str(data[x]['Destination']) +" "+  str(data[x]['Min'])
-                        if x < (len(data)-1):
-                                s+="\n"
+        for x in data:
+                y+=1
+                try:
+                        #I can't make it to the station in < 6 minutes!
+                        if int(x['Min'])>=6:
+                                s+=str(x['Destination']) +" "+  str(x['Min'])
+                                if y < (len(data)):
+                                        s+="\n"    
+                #Handle all non-int cases with a pass (ARR, BRD, ---, etc)
+                except ValueError:
+                        pass
         proc.communicate(s) #Uncomment before deploying on PI
-        #print(s) - For testing 
-
+        #print(s) #- For testing
 except IOError:
         s+="IO Error!"
         s+="\n"
